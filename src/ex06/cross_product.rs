1db939dfcabb34c2112b7d3e9e78277fa92de097
abourbou <arthuro6996@live.fr>
2023-05-11 22:16:26 +0200

    [refact] Refact ex06 with template for scalar

---
+++ src/ex06/cross_product.rs
@@ -0,0 +1,30 @@
+
+use crate::base_struct::{scalar::Scalar, vector::Vector};
+use std::ops::{Sub, Mul};
+
+pub fn cross_product<T : Scalar>(u: &Vector<T,3>, v: &Vector<T,3>) -> Vector<T,3>
+	where T : Sub<T, Output = T> + Mul<T, Output = T> {
+	Vector::from([	u.arr[1] * v.arr[2] - u.arr[2] * v.arr[1],
+					u.arr[2] * v.arr[0] - u.arr[0] * v.arr[2],
+					u.arr[0] * v.arr[1] - u.arr[1] * v.arr[0]])
+}
+
+#[cfg(test)]
+	#[test]
+	fn test00() {
+		let u = Vector::from([0., 0., 1.]);
+		let v = Vector::from([1., 0., 0.]);
+		assert_eq!(cross_product::<f32>(&u, &v), Vector::from([0., 1., 0.]));
+	}
+	#[test]
+	fn test01() {
+		let u = Vector::from([1., 2., 3.]);
+		let v = Vector::from([4., 5., 6.]);
+		assert_eq!(cross_product::<f32>(&u, &v), Vector::from([-3., 6., -3.]));
+	}
+	#[test]
+	fn test02() {
+		let u = Vector::from([4., 2., -3.]);
+		let v = Vector::from([-2., -5., 16.]);
+		assert_eq!(cross_product::<f32>(&u, &v), Vector::from([17., -58., -16.]));
+	}
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod ex02;
 pub mod ex03;
 pub mod ex04;
 pub mod ex05;
-// pub mod ex06;
+pub mod ex06;
 // pub mod ex07;
 // pub mod ex08;
 // pub mod ex09;
