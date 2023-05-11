6baec55117d6a6c73996dbfffb34b2edd90e24a4
abourbou <arthuro6996@live.fr>
2023-05-11 22:13:05 +0200

    [fix] Fix ex01 which is not a member of vector

--- src/ex01/linear_combination.rs
+++ src/ex01/linear_combination.rs
@@ -2,9 +2,8 @@
 use crate::base_struct::{scalar::Scalar, vector::Vector};
 use std::ops::{Add, Mul};
 
-impl<T : Scalar, const M: usize> Vector<T, M>
+pub fn linear_combination<T: Scalar, const M : usize>(u: &[Vector<T,M>], coeffs: &[T]) -> Vector<T,M>
 	where T: Add<T,Output=T> + Mul<T,Output=T>{
-	pub fn linear_combination(u: &[Self], coeffs: &[T]) -> Self{
 		if u.is_empty() {
 			panic!("empty list of vector");
 		}
@@ -12,17 +11,17 @@ impl<T : Scalar, const M: usize> Vector<T, M>
 			panic!("list coeff need to be the same size that the list of vector");
 		}
 
-		let mut vec_lincb = Self::default();
+		let mut vec_lincb = Vector::<T,M>::default();
 		for i in  0..M {
-			let mut buffer_value : T = T::zero();
+			let mut buffer_value = T::zero();
 			for j in 0..u.len() {
 				buffer_value = coeffs[j] * u[j].arr[i] + buffer_value;
 			}
-		vec_lincb.arr[i] = buffer_value;
+			vec_lincb.arr[i] = buffer_value;
+		}
+		vec_lincb
 	}
-	vec_lincb
-	}
-}
+
 
 #[cfg(test)]
 	#[test]
@@ -31,7 +30,7 @@ impl<T : Scalar, const M: usize> Vector<T, M>
 		let vec2 = Vector::<f32,3>::new(2.0);
 		let u = [vec, vec2];
 		let coeffs = [2.0, 1.0];
-		assert_eq!(Vector::<f32,3>::linear_combination(&u, &coeffs), Vector::from([4.0, 6.0, 8.0]));
+		assert_eq!(linear_combination::<f32,3>(&u, &coeffs), Vector::from([4.0, 6.0, 8.0]));
 	}
 	#[test]
 	fn linear_comb_test01() {
@@ -39,7 +38,7 @@ impl<T : Scalar, const M: usize> Vector<T, M>
 		let vec2 = Vector::<f32,2>::new(2.0);
 		let u = [vec, vec2];
 		let coeffs = [0.0, 3.0];
-		assert_eq!(Vector::<f32,2>::linear_combination(&u, &coeffs), Vector::from([6.0, 6.0]));
+		assert_eq!(linear_combination::<f32,2>(&u, &coeffs), Vector::from([6.0, 6.0]));
 	}
 	#[test]
 	#[should_panic]
@@ -48,12 +47,12 @@ impl<T : Scalar, const M: usize> Vector<T, M>
 		let vec2 = Vector::<f32,2>::new(2.0);
 		let u = [vec, vec2];
 		let coeffs = [1.0];
-		assert_eq!(Vector::<f32,2>::linear_combination(&u, &coeffs), Vector::from([6.0, 6.0]));
+		assert_eq!(linear_combination::<f32,2>(&u, &coeffs), Vector::from([6.0, 6.0]));
 	}
 	#[test]
 	#[should_panic]
 	fn linear_comb_test03() {
 		let u = [];
 		let coeffs = [1.0];
-		assert_eq!(Vector::<f32,2>::linear_combination(&u, &coeffs), Vector::from([6.0, 6.0]));
+		assert_eq!(linear_combination::<f32,2>(&u, &coeffs), Vector::from([6.0, 6.0]));
 	}
