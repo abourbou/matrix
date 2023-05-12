c76424b2f496642aad08b39fa577dc59796f93cc
abourbou <abourbou@z2r12p4.42lyon.fr>
2023-05-12 14:52:50 +0200

    [refact] Refact ex10 and 11 with template for scalar

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 
+#[macro_use]
+extern crate assert_float_eq;
+
 pub mod base_struct;
 pub mod ex00;
 pub mod ex01;
@@ -10,8 +13,8 @@ pub mod ex06;
 pub mod ex07;
 pub mod ex08;
 pub mod ex09;
-// pub mod ex10;
-// pub mod ex11;
+pub mod ex10;
+pub mod ex11;
 // pub mod ex12;
 // pub mod ex13;
 // pub mod ex14;
